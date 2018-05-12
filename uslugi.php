8ec1fd9d68027d5f3f0bed27db20aef53e009085
AnnSholomova <31933664+AnnSholomova@users.noreply.github.com>
2018-05-12 17:39:46 +0300

    Update uslugi.php

--- uslugi.php
+++ uslugi.php
@@ -2,23 +2,23 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>Автосервис : Прайс-лист запчастей</title>
+		<title>Автосервис : Прайс-лист услуг</title>
 		<style>
-			zapchasti {
+			uslugi {
 				display: table;
 			}
-			zapchast {
+			usluga {
 				display: table-row;
 			}
-			zapchasti > form {
+			uslugi > form {
 				display: table-row;
 			}
-			zapchasti > form > input {
+			uslugi > form > input {
 				display: table-cell;
 				padding: 10px;
 				border: 1px solid darkblue;
 			}
-			zapchast > * {
+			usluga > * {
 				display: table-cell;
 				padding: 10px;
 				border: 1px solid darkblue;
@@ -36,36 +36,36 @@ class MyDB extends SQLite3
     }
 }
 $db = new MyDB();
-$result = $db->query('SELECT rowid, name, number, price 
-                      FROM zapchasti 
+$result = $db->query('SELECT rowid, name, price_car, price_jeep
+                      FROM uslugi 
                       ORDER BY name');
-echo "<form action='edit_zch.php' method='post'>\n";
+echo "<form action='edit_usl.php' method='post'>\n";
 echo "<input name='id'     
              placeholder='№' />\n";
 echo "<input name='name'   
 			 placeholder='Наименование' />\n";
-echo "<input name='number' 
-             placeholder='Число' />\n";
-echo "<input name='price'  
-             placeholder='Цена' />\n";
+echo "<input name='price_car' 
+             placeholder='Цена для легковых автомобилей' />\n";
+echo "<input name='price_jeep'  
+             placeholder='Цена для джипов и минивенов' />\n";
 echo "<input name='new'
 			 value='Создать'
 			 type='submit' />\n";
 echo "</form>\n";
 while ($row = $result->fetchArray())
 {
-	echo "<form action='edit_zch.php' method='post'>\n";
+	echo "<form action='edit_usl.php' method='post'>\n";
 	echo "<input name='id'     
 	             value='".$row['rowid']."'
 	             placeholder='id' />\n";
 	echo "<input name='name'   
 				 value='".$row['name']."'   
 				 placeholder='id' />\n";
-	echo "<input name='number' 
-	             value='".$row['number']."' 
+	echo "<input name='price_car' 
+	             value='".$row['price_car']."' 
 	             placeholder='id' />\n";
-	echo "<input name='price'  
-	             value='".$row['price']."'  
+	echo "<input name='price_jeep'  
+	             value='".$row['price_jeep']."'  
 	             placeholder='id' />\n";
 	echo "<input name='delete'
 				 value='Удалить'
@@ -75,7 +75,7 @@ while ($row = $result->fetchArray())
 				 type='submit' />\n";
 	echo "</form>\n";
 }
-echo "</zapchasti>\n";
+echo "</uslugi>\n";
 ?>
 	</body>
 </html>

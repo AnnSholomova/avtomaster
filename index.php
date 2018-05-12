a1b0a4cfeba74dd3a4412c44f78178aafd4d3d8a
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-05-12 09:51:32 +0500

    form

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Автосервис : Вход в систему</title>
+	</head>
+	<body>
+		<form action="login.php" method="post">
+			<input type="text" name="login" value="" />
+			<input type="password" name="pwd" value="" />
+			<input type="submit" name="" value="Вход в систему" />
+		</form>
+	</body>
+</html>
+
+
